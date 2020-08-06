2585ca4a69938d18d3ad5de5bd5bc5dcbf809e25
Tom <thomas.spencer@codurance.com>
2020-08-06 22:12:24 +0100

    Add List for AndSpecBuilder

--- 4-Completed-Interpreter/src/main/java/pl/refactoring/interpreter/completed/spec/AndSpecBuilder.java
+++ 4-Completed-Interpreter/src/main/java/pl/refactoring/interpreter/completed/spec/AndSpecBuilder.java
@@ -1,17 +1,20 @@
 package pl.refactoring.interpreter.completed.spec;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import pl.refactoring.interpreter.completed.Spec;
 
 public class AndSpecBuilder {
 
-  private Spec[] specs;
+  private List<Spec> specs = new ArrayList<>();
 
   public AndSpecBuilder withSpecs(Spec... specs) {
-    this.specs = specs;
+    this.specs = Arrays.asList(specs);
     return this;
   }
 
   public AndSpec createAndSpec() {
-    return new AndSpec(specs);
+    return new AndSpec(specs.toArray(new Spec[specs.size()]));
   }
 }
